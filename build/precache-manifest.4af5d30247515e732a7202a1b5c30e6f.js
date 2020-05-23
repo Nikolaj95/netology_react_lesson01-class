5855e6abf2a0d32cfa25a2453eb44da4f3abee89
Nikolay Konyukov <konyukov142@gmail.com>
2020-05-23 23:18:23 +0700

    задание 2

---
+++ build/precache-manifest.4af5d30247515e732a7202a1b5c30e6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df10d32b349f268bca58b567b079c16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bffb7c9710b7ce6d74cd",
+    "url": "/static/css/main.ade75065.chunk.css"
+  },
+  {
+    "revision": "65602e0efd56a2cdc2e2",
+    "url": "/static/js/2.4f85f8f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4f85f8f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffb7c9710b7ce6d74cd",
+    "url": "/static/js/main.b760e541.chunk.js"
+  },
+  {
+    "revision": "cd4c515c40988de6c31b",
+    "url": "/static/js/runtime-main.b5903f53.js"
+  },
+  {
+    "revision": "9c63a2c92146aa97904b9148bc055ce3",
+    "url": "/static/media/item-black.9c63a2c9.jpg"
+  },
+  {
+    "revision": "8bcbdffead0d5ba6700c1551189af1b8",
+    "url": "/static/media/item-blue.8bcbdffe.jpg"
+  }
+]);
---
+++ src/components/ShopItemClass.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import StoryItem from "../models/StoryItem";
+
+export default class MyComponent extends Component {
+        static propTypes = {
+        ShopItem: PropTypes.instanceOf(StoryItem),
+    }
+    render() {
+        let {item} = this.props;
+        return (
+            <>
+                <div className="main-content">
+                    <h2>{item.brand}</h2>
+                    <h1>{item.title}</h1>
+                    <h3>{item.description}</h3>
+                    <div className="description">
+                        {item.descriptionFull}
+                    </div>
+                    <div className="highlight-window mobile">
+                        <div className="highlight-overlay"></div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="purchase-info">
+                        <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+                        <button>Добавить в корзину</button>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
